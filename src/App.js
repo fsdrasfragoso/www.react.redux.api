9fb788223dd10db6dc02fdd3b2e3f21fc540f518
esdrasneto <esdras.neto@atiovo.com>
2021-11-26 00:24:18 -0300

    mudança de estado no formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
+import photo from './perfil.jpeg'; 
+import { Component } from 'react';
+class App extends Component
+{
+  constructor(props)
+  {
+    super(props)
+      this.state = {
+        name: "Esdras",
+        email:"esdrasfragoso@gmail.com"
+      }
+      this.changeState = this.changeState.bind(this)
+      this.resetState = this.resetState.bind(this)
+      this.changeInput = this.changeInput.bind(this)
+  }
+  
+  changeState()
+  {
+     this.setState({
+       name:"Esdras Fragoso"
+     })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  resetState()
+  {
+     this.setState({
+       name:"Esdras"
+     })
+  }
+  changeInput(event)
+  {
+    let target = event.target;
+    let index = target.name
+    this.setState({
+      [index]:target.value
+    })
+  }
+
+  render() 
+  {
+    return (
+      <div className="App">
+        <div>
+          <form>
+            <label>
+              Nome
+              <input 
+                type="text" 
+                name="name"
+                value={this.state.name}
+                onChange={this.changeInput}
+              ></input>
+            </label>
+            <label>
+              Email
+              <input 
+                type="text" 
+                name="email"
+                value={this.state.email}
+                onChange={this.changeInput}
+              ></input>
+            </label>
+          </form>
+          {this.state.name} - {this.state.email}
+        </div>
+        <button onClick={this.changeState}> Mudar estado</button>
+        <button onClick={this.resetState}> Resetar Estado</button>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
